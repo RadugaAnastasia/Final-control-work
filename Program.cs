d22d32145807436eb524d37d21fb9a6ddc95e79a
Anastasiia <martishka85@list.ru>
2023-11-01 18:05:08 +0300

    Написан код, проверен на работоспособность

--- Program.cs
+++ Program.cs
@@ -2,4 +2,42 @@
 // длина которых меньше, либо равна 3 символам.
 // Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма.
 // При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+// Примеры:
+// [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
+// [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
+// [“Russia”, “Denmark”, “Kazan”] → []
 
+Console.Clear(); // очистка экрана
+string[] array1 = new string[4]{"Hello", "2", "world", ":-)"};
+// string[] array1 = new string[4]{"1234", "1567", "-2", "computer science"};
+// string[] array1 = new string[3]{"Russia", "Denmark", "Kazan"};
+string[] array2 = new string[array1.Length];
+Array3ElFind(array1,array2);
+PrintArray(array1);
+Console.Write(" -> ");
+PrintArray(array2);
+
+
+void Array3ElFind(string[] array1, string[] array2) //поиск строки из 3х эл-тов
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i].Length <= 3)
+        {
+            array2[count] = array1[i];
+            count++;
+        }
+    }
+}
+
+void PrintArray(string[] array) //печать массива
+{
+    Console.Write("[ ");
+    for (int i = 0; i < array.Length-1; i++)
+    {
+        Console.Write($"{array[i]}  ");
+    }
+    Console.Write($"{array[array.Length-1]}");
+    Console.Write(" ]");
+}
